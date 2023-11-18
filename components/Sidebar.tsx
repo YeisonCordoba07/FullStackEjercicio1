c5ec6e6ba29a2420dfdf564a4234b2525ab10731
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2023-11-18 00:27:18 -0500

    Arreglar sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,15 +1,16 @@
 import { BotonEnlace } from "@/components/BotonEnlace";
 import Image from "next/image";
 
-const Sidebar = () =>{
-    return(
+const Sidebar = () => {
+  return (
+    <div className="w-96 relative h-screen bg-emerald-500">
+      <div className="fixed bg-emerald-300 flex justify-center  flex-col left-0 p-5 w-96 h-screen gap-10">
         
-        <div className="flex flex-col gap-10 bg-emerald-300 h-full w-96 p-5 relative z-20 after:absolute after:left-full after:top-0 after:z-10
-        ">
-        <div className="flex gap-4 flex-col items-center relative">
+        {/* Div para Imagenes y texto */}
+        <div className="flex gap-4 flex-col items-center  justify-center">
           <Image
             className="rounded-full"
-            src="/cell.png"
+            src="/cell2.jpg"
             height={300}
             width={300}
             alt="Foto de perfil"
@@ -17,15 +18,15 @@ const Sidebar = () =>{
           <h2>Nombre del usuario</h2>
         </div>
 
-        <div className="gap-4 flex flex-col">
+        {/* Div para Botones  */}
+        <div className="gap-4 flex flex-col w-full">
           <BotonEnlace href="#" titulo="Inventarios" />
           <BotonEnlace href="#" titulo="Materiales" />
           <BotonEnlace href="#" titulo="usuarios" />
         </div>
-
       </div>
-        
-    );
-}
+    </div>
+  );
+};
 
-export {Sidebar};
+export { Sidebar };
